a0cbed11fb6edb4861f91ad7fbbcf572f8e1930c
Noel Ramadan <dreamxscape.management@gmail.com>
2025-07-21 09:19:39 -0600

    some config changes

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: process.env.NODE_ENV === 'production' ? '/YOUR_REPOSITORY_NAME' : '',
   images: {
     unoptimized: true, // Disable Next.js image optimization
   },
