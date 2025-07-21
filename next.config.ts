e8944b5ebb3a711db44927d975583dda12af6c74
Noel Ramadan <dreamxscape.management@gmail.com>
2025-07-21 09:20:04 -0600

    typo lol

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
-  basePath: process.env.NODE_ENV === 'production' ? '/YOUR_REPOSITORY_NAME' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/single-page-demo' : '',
   images: {
     unoptimized: true, // Disable Next.js image optimization
   },
