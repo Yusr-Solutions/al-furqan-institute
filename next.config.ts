26e1591ec42543c52fa0b4223129f5cf2daf845c
Noel Ramadan <dreamxscape.management@gmail.com>
2025-09-01 19:49:48 -0600

    Some changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
-  basePath: process.env.NODE_ENV === 'production' ? '/single-page-demo' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/al-furqan-insitute' : '',
   images: {
     unoptimized: true, // Disable Next.js image optimization
   },
